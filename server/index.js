a40a6681ee4f2cc6cde82978ad88a26aa1684b21
ankitsharmma <sharmaankit7090@gmail.com>
2025-01-22 09:00:34 +0530

    second_commit

--- server/index.js
+++ server/index.js
@@ -19,12 +19,22 @@ const PORT = process.env.PORT || 3000;
 
 // default middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.FRONTEND_URL,
     credentials:true
+
+
+   
 }));
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+// routes
+
+
  
 // apis
 app.use("/api/v1/media", mediaRoute);
